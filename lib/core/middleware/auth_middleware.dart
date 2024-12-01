96a00af8ce4b846c9287e2d9f829e97a6093f33c
matthewyu <torry0402@gmail.com>
2024-12-01 20:08:44 +0800

    新增用戶註冊功能

--- lib/core/middleware/auth_middleware.dart
+++ lib/core/middleware/auth_middleware.dart
@@ -7,23 +7,59 @@ class AuthMiddleware {
   /// 取得系統 jwt key
   final secretKey = Env.jwtSecretKey;
 
-  /// 生成 jwt Token
-  String generateJWT(Map<String, dynamic> payload) {
-    // jwt 7天過期
-    final expiration = DateTime.now().add(Duration(days: 7));
+  /// 生成驗證 Token(時間短)
+  String generateAccessToken(Map<String, dynamic> payload) {
+    // jwt 2小時過期
+    final expiration = DateTime.now().add(Duration(hours: 2));
     payload['exp'] = expiration.millisecondsSinceEpoch ~/ 1000;
+    //  添加標籤
+    payload['type'] = 'access';
 
-    //  使用jwt 生成 token
+    //  使用 jwt 生成 token
     final jwt = JWT(payload);
     return jwt.sign(SecretKey(secretKey));
+  }
+
+  /// 生成刷新 token(時間長)
+  String generateRefreshToken(Map<String, dynamic> payload) {
+    // jwt 30天過期
+    final expiration = DateTime.now().add(Duration(days: 30));
+    payload['exp'] = expiration.millisecondsSinceEpoch ~/ 1000;
+    // 添加標籤
+    payload['type'] = 'refresh';
+
+    // 使用 jwt 生成 token
+    final jwt = JWT(payload);
+    return jwt.sign(SecretKey(secretKey));
+  }
+
+  /// 用 refreshToken 刷新 AccessToken
+  String refreshAccessToken(String refreshToken) {
+    try {
+      // 驗證 refreshToken
+      final jwt = JWT.verify(refreshToken, SecretKey(secretKey));
+
+      if (jwt.payload['type'] != 'refresh') {
+        throw 'Invaild token type';
+      }
+
+      // 從新生成 payload，並且移除掉 refresh token 的 exp 和 type
+      final payload = Map<String, dynamic>.from(jwt.payload)
+        ..remove('exp')
+        ..remove('type');
+
+      return generateAccessToken(payload);
+    } catch (e) {
+      throw '無效token,錯誤碼為$e';
+    }
   }
 
   /// 驗證 JWT
   bool verifyJWT(String token) {
     try {
       // 解析 token
-      JWT.verify(token, SecretKey(secretKey));
-      return true;
+      final jwt = JWT.verify(token, SecretKey(secretKey));
+      return jwt.payload['type'] == 'access';
     } catch (e) {
       return false;
     }
--- lib/shared/services/auth_service.dart
+++ lib/shared/services/auth_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:mysql1/mysql1.dart';
 import 'package:bcrypt/bcrypt.dart';
 import 'package:task_management/shared/models/users.dart';
@@ -5,9 +7,13 @@ import 'package:task_management/core/config/database_config.dart';
 import 'package:task_management/core/middleware/auth_middleware.dart';
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:task_management/core/config/env.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:logging/logging.dart';
 
 class AuthService {
   late final MySqlConnection _db;
+
+  final _logger = Logger('AuthService');
 
   /// 從config 取得連線資料
   Future<void> connect() async {
@@ -42,14 +48,27 @@ class AuthService {
         'role': user.role,
       };
 
-      final token = AuthMiddleware().generateJWT(payload);
+      final accessToken = AuthMiddleware().generateAccessToken(payload);
+      final refreshToken = AuthMiddleware().generateRefreshToken(payload);
 
       return {
-        'token': token,
+        'accessToekn': accessToken,
+        'refreshToken': refreshToken,
         'user': user.toMap()..remove('password_hash'),
       };
     } catch (e) {
+      _logger.warning('登入失敗', e);
       rethrow;
+    }
+  }
+
+  /// 刷新 accessToken
+  String refreshAccessToken(String refreshToken) {
+    try {
+      return AuthMiddleware().refreshAccessToken(refreshToken);
+    } catch (e) {
+      _logger.warning('刷新token失敗', e);
+      throw '刷新token失敗';
     }
   }
 
@@ -58,8 +77,109 @@ class AuthService {
     try {
       return BCrypt.checkpw(password, passwordHash);
     } catch (e) {
-      print('密碼驗證失敗 $e');
+      _logger.warning('密碼驗證失敗', e);
       return false;
     }
+  }
+
+  /// 用戶註冊
+  Future<User> register(User user, String plainPassword) async {
+    try {
+      final checkUserName = await _db.query(
+        'SELECT id FROM users WHERE username = ?',
+        [user.username],
+      );
+
+      if (checkUserName.isNotEmpty) {
+        throw '用戶名重複,請重新輸入';
+      }
+
+      if (!emailverify(user.email)) {
+        throw 'Email格式錯誤';
+      }
+
+      final checkEmail = await _db.query(
+        'SELECT id FROM users WHERE email = ?',
+        [user.email],
+      );
+
+      if (checkEmail.isNotEmpty) {
+        throw 'Email重複,請重新輸入';
+      }
+
+      if (!validatePassword(user.passwordHash)) {
+        throw '密碼至少8位,包含大小寫字母、數字';
+      }
+
+      // 生成 hash
+      final salt = BCrypt.gensalt();
+      final passwordHash = BCrypt.hashpw(plainPassword, salt);
+
+      // 建立新的 user 物件，包含 hash 後的密碼
+      final userToSave = user.copyWith(
+          passwordHash: passwordHash,
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now());
+
+      final result = await _db.query('''
+      INSERT INTO users (
+        username,
+        email,
+        password_hash,
+        display_name,
+        avatar_url,
+        phone,
+        language,
+        theme,
+        time_zone,
+        notification_settings,
+        is_active,
+        created_at,
+        updated_at,
+        role
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', [
+        userToSave.username,
+        userToSave.email,
+        userToSave.passwordHash,
+        userToSave.displayName,
+        userToSave.avatarUrl,
+        userToSave.phone,
+        userToSave.language,
+        userToSave.theme,
+        userToSave.timeZone,
+        json.encode(userToSave.notificationSettings),
+        userToSave.isActive ? 1 : 0,
+        userToSave.createdAt.toIso8601String(),
+        userToSave.updatedAt.toIso8601String(),
+        userToSave.role,
+      ]);
+
+      // 返回包含 ID 的用戶資料
+      return userToSave.copyWith(id: result.insertId);
+    } catch (e) {
+      // 註冊失敗 rollback
+      await _db.query('ROLLBACK');
+      _logger.warning(e);
+      throw '註冊失敗: $e';
+    }
+  }
+
+  /// 驗證 email(同步)
+  bool emailverify(String email) {
+    try {
+      return EmailValidator.validate(email);
+    } catch (e) {
+      _logger.warning(e);
+      throw 'Email格式不正確，請確認格式是否正確，例：example@mail.com';
+    }
+  }
+
+  /// 驗證密碼複雜度
+  bool validatePassword(String password) {
+    // 至少8位，包含大小寫字母、數字
+    final passwordRegExp =
+        RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$');
+    return passwordRegExp.hasMatch(password);
   }
 }
